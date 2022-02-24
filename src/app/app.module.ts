3c16f6f05c4db3616a8b65b0d30b86e9842437c3
Antoine Mottier <team.adict@adict-solutions.eu>
2022-02-24 17:04:38 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './home/categories/categories.component';
+import { CardsComponent } from './categories/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoriesComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
