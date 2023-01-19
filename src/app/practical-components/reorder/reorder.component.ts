124ab6ed88dab7c5347e5d567c8934fa640d2946
Antoine Mottier <team.adict@adict-solutions.eu>
2023-01-19 10:42:49 +0100

    new things

--- src/app/practical-components/reorder/reorder.component.ts
+++ src/app/practical-components/reorder/reorder.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BasicPracticalComponentService } from 'src/app/services/basic-practical-component.service';
 import { trigger, state, style, animate, transition } from '@angular/animations';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, } from '@angular/cdk/drag-drop';
 import { BasicToSortItem } from '../basic-drop-item';
 
 @Component({
@@ -24,27 +24,64 @@ export class ReorderComponent implements OnInit {
   goodOrder: boolean = true;
   orderChecked: boolean = false;
 
+
+  base_items: any = []
+  trash_items: any = []
+
   ngOnInit(): void {
+    this.base_items = this.data.choices
   }
 
-  constructor(private practicalService : BasicPracticalComponentService) { 
-    };
-   
+  constructor(private practicalService: BasicPracticalComponentService) {
+  };
 
-  drop(event: CdkDragDrop<BasicToSortItem[]>) { moveItemInArray(this.data.choices, event.previousIndex, event.currentIndex); 
-  this.orderChecked=false;
-  this.goodOrder=true;
-  this.badOrder=false;
+
+  // drop(event: CdkDragDrop<BasicToSortItem[]>) { 
+
+  //   if (event.previousContainer === event.container) {
+  //   moveItemInArray(this.data.choices, event.previousIndex, event.currentIndex); }
+  //   else{
+  //     transferArrayItem( event.previousContainer.data,this.data.choices, event.previousIndex, event.currentIndex)
+
+  //   }
+
+
+  // this.orderChecked=false;
+  // this.goodOrder=true;
+  // this.badOrder=false;
+  // }
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      console.log(event.container.data)
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+      console.log(event.currentIndex)
+    }
+    this.orderChecked = false;
+    this.goodOrder = true;
+    this.badOrder = false;
   }
 
   checkOrder() {
     for (let i = 0; i < Object.keys(this.data.choices).length; i++) {
-      let defined_order = (this.data.choices[i].order)
-      if (defined_order != i+1) {
+      if (this.data.choices[i].is_bad_step) {
         this.badOrder = true;
-        this.goodOrder=false;
+        this.goodOrder = false;
       }
-      this.orderChecked=true;
+      let defined_order = (this.data.choices[i].order)
+      if (defined_order != i + 1) {
+        this.badOrder = true;
+        this.goodOrder = false;
+      }
+      this.orderChecked = true;
     }
   }
 
